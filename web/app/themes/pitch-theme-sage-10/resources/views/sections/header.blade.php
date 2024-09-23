159bbe5f98eb8653432c994d4f7ba514e98ad234
apeillon-pitch <apeillon@lepitch.fr>
2024-09-23 16:35:40 +0200

    Update header.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/sections/header.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/sections/header.blade.php
@@ -39,7 +39,7 @@
             <a href="{!! $header['data']['facebook']['url'] !!}"
                aria-label="{!! $header['data']['facebook']['title'] !!}"
                target="{!! $header['data']['facebook']['target'] !!}">
-              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-square-facebook"></i>
             </a>
           @endif
           @if($header['data']['instagram'])
